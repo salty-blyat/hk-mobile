b837d3078859c4b2dc1b43b86344341f07d64f21
devitrithy <devitrithy1@gmail.com>
2025-01-03 23:55:45 +0700

    use translate from core

--- lib/utils/translation.dart
+++ lib/utils/translation.dart
@@ -1,9 +1,13 @@
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 
 class Translate extends Translations {
+  final dio = Dio();
+  final secureStorage = const FlutterSecureStorage();
   @override
   Map<String, Map<String, String>> get keys => translations;
 
@@ -11,11 +15,21 @@ class Translate extends Translations {
 
   Future<void> loadTranslations() async {
     for (var lang in ['en', 'km']) {
-      final String jsonString =
-          await rootBundle.loadString('assets/langs/$lang.json');
-      final Map<String, dynamic> jsonMap = json.decode(jsonString);
-      translations[lang] =
-          jsonMap.map((key, value) => MapEntry(key, value.toString()));
+      var res = await dio.get('https://core.sgx.bz/files/langs/sp-$lang.json');
+
+      if (res.statusCode == 200) {
+        // final String jsonString =
+        //     await rootBundle.loadString('assets/langs/$lang.json');
+        final Map<String, dynamic> jsonMap = res.data;
+        translations[lang] =
+            jsonMap.map((key, value) => MapEntry(key, value.toString()));
+      } else {
+        final String jsonString =
+            await rootBundle.loadString('assets/langs/$lang.json');
+        final Map<String, dynamic> jsonMap = json.decode(jsonString);
+        translations[lang] =
+            jsonMap.map((key, value) => MapEntry(key, value.toString()));
+      }
     }
   }
 }
