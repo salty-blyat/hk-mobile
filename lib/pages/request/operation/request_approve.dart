c86b569ed67e62c113f596ea678f5d3750c9af05
Panha <panhat938@gmail.com>
2025-01-27 17:43:11 +0700

    next approver

--- lib/pages/request/operation/request_approve.dart
+++ lib/pages/request/operation/request_approve.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 import 'package:staff_view_ui/pages/request/operation/request_operation_controller.dart';
+import 'package:staff_view_ui/pages/staff/staff_select.dart';
 import 'package:staff_view_ui/utils/theme.dart';
 import 'package:staff_view_ui/utils/widgets/button.dart';
 
@@ -15,33 +16,104 @@ class ApproveRequest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     controller.formGroup.patchValue({'id': id});
-    return ReactiveForm(
-      formGroup: controller.formGroup,
-      child: Column(
-        children: [
-          ReactiveTextField<String>(
-            formControlName: 'note',
-            maxLines: 3,
-            decoration: InputDecoration(
-              labelText: '${'Note'.tr} (${'Optional'.tr})',
+
+    controller.isNextApprove.value = false;
+    return Obx(() {
+      return !controller.isNextApprove.value
+          ? _buildApprove()
+          : _buildNextApprove();
+    });
+  }
+
+  Widget _buildApprove() {
+    return Column(
+      children: [
+        ReactiveForm(
+          formGroup: controller.formGroup,
+          child: Column(
+            children: [
+              ReactiveTextField<String>(
+                formControlName: 'note',
+                maxLines: 3,
+                decoration: InputDecoration(
+                  labelText: '${'Note'.tr} (${'Optional'.tr})',
+                ),
+              ),
+              const SizedBox(height: 16),
+              MyButton(
+                onPressed: controller.approve,
+                loading: controller.loading.value,
+                label: 'Approve'.tr,
+                color: AppTheme.successColor,
+              ),
+              const SizedBox(height: 8),
+              MyButton(
+                onPressed: () {
+                  controller.showNextApprover();
+                },
+                label: 'Send to next approver'.tr,
+                color: Colors.grey.shade200,
+                textColor: Colors.black87,
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildNextApprove() {
+    controller.formGroup.control('nextApproverId').markAsEnabled();
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.topLeft,
+          child: IconButton(
+            style: ButtonStyle(
+              padding: WidgetStateProperty.all(EdgeInsets.zero),
+            ),
+            onPressed: () {
+              controller.hideNextApprover();
+            },
+            icon: const Icon(
+              Icons.arrow_back,
+              color: AppTheme.primaryColor,
+              size: 28,
             ),
           ),
-          const SizedBox(height: 16),
-          MyButton(
-            onPressed: controller.approve,
-            loading: controller.loading.value,
-            label: 'Approve'.tr,
-            color: AppTheme.successColor,
+        ),
+        const SizedBox(height: 16),
+        ReactiveForm(
+          formGroup: controller.formGroup,
+          child: Column(
+            children: [
+              ReactiveTextField<String>(
+                formControlName: 'note',
+                maxLines: 3,
+                decoration: InputDecoration(
+                  labelText: '${'Note'.tr} (${'Optional'.tr})',
+                ),
+              ),
+              const SizedBox(height: 16),
+              StaffSelect(
+                formControlName: 'nextApproverId',
+                formGroup: controller.formGroup,
+                labelText: 'Next Approver'.tr,
+              ),
+              const SizedBox(height: 16),
+              Obx(() {
+                return MyButton(
+                  onPressed: controller.nextApprove,
+                  disabled: !controller.formValid.value,
+                  loading: controller.loading.value,
+                  label: 'Submit'.tr,
+                  color: AppTheme.primaryColor,
+                );
+              })
+            ],
           ),
-          const SizedBox(height: 8),
-          MyButton(
-            onPressed: () {},
-            label: 'Send to next approver'.tr,
-            color: Colors.grey.shade200,
-            textColor: Colors.black87,
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
--- lib/pages/request/operation/request_operation_controller.dart
+++ lib/pages/request/operation/request_operation_controller.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:reactive_forms/reactive_forms.dart';
+import 'package:staff_view_ui/helpers/common_validators.dart';
 import 'package:staff_view_ui/pages/request/request_controller.dart';
 import 'package:staff_view_ui/pages/request/request_service.dart';
 import 'package:staff_view_ui/pages/request/view/request_view_controller.dart';
@@ -12,18 +13,55 @@ class RequestOperationController extends GetxController {
   final RequestApproveController requestApproveController =
       Get.put(RequestApproveController());
   final loading = false.obs;
+  final isNextApprove = false.obs;
+  final formValid = false.obs;
 
   final formGroup = FormGroup({
     'note': FormControl<String>(
       value: '',
     ),
-    'nextApproverId': FormControl<int>(value: 0),
+    'nextApproverId': FormControl<int>(
+      value: null,
+      disabled: true,
+      validators: [
+        Validators.delegate(CommonValidators.required),
+      ],
+    ),
     'id': FormControl<int>(
       value: 0,
     ),
   });
 
+  @override
+  void onInit() {
+    formGroup.valueChanges.listen((value) {
+      formValid.value = formGroup.valid;
+    });
+
+    super.onInit();
+  }
+
   void approve() {
+    formGroup.markAsTouched();
+    formGroup.control('nextApproverId').patchValue(0);
+    if (!loading.value) {
+      loading.value = true;
+      Modal.loadingDialog();
+      requestService.approve(formGroup.rawValue).then((value) {
+        if (value.statusCode == 200) {
+          requestViewController.checkCanDoAction();
+          requestViewController.findById(formGroup.value['id'] as int);
+          requestApproveController.search();
+          Get.back();
+          Get.back();
+          loading.value = false;
+        }
+      });
+    }
+    loading.value = false;
+  }
+
+  void nextApprove() {
     formGroup.markAsTouched();
     if (!loading.value) {
       loading.value = true;
@@ -77,5 +115,13 @@ class RequestOperationController extends GetxController {
       });
     }
     loading.value = false;
+  }
+
+  void showNextApprover() {
+    isNextApprove.value = true;
+  }
+
+  void hideNextApprover() {
+    isNextApprove.value = false;
   }
 }
